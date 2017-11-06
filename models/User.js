9b224cf6c34449f5d719e991ed003fe0b00ba019
Jason Rouse <jrrouse88@gmail.com>
2017-11-06 10:35:00 -0800

    fixed spelling in userSchema

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const
   userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
-    username: {type: String, unique: true},
+    userName: {type: String, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
   })
