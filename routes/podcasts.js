75c61add38e1fa606642a0e168f640872bd56bd6
Jason Rouse <jrrouse88@gmail.com>
2017-11-06 12:19:34 -0800

    updated podcasts router with episode routes

--- routes/podcasts.js
+++ routes/podcasts.js
@@ -1,11 +1,19 @@
 const
   express = require('express'),
   podcastsRouter = new express.Router(),
-  podcastsCtrl = require('../controllers/podcasts.js')
+  podcastsCtrl = require('../controllers/podcasts.js'),
+  episodesCtrl = require('../controllers/episodes.js')
 ;
 
 podcastsRouter.get('/', podcastsCtrl.index)
 
 podcastsRouter.get('/:castId', podcastsCtrl.show)
+
+podcastsRouter.post('/:castId/episodes', episodesCtrl.create)
+
+podcastsRouter.route('/:castId/episodes/:episodeId')
+  .patch(episodesCtrl.update)
+  .delete(episodesCtrl.destroy)
+;
 
 module.exports = podcastsRouter
