1e25085a8b7a2546dec691c90e961944af12cc07
Jason Rouse <jrrouse88@gmail.com>
2017-11-06 15:01:06 -0800

    created navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
 
+import NavBar from './NavBar.jsx'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <NavBar />
       </div>
     );
   }
---
+++ client/src/NavBar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = (props) => {
+  return (
+    <div className='navbar'>
+      This is the nav bar
+    </div>
+  )
+}
+
+export default NavBar
