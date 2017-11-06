8b1cc6d469d00732443a8458dc99b995ea9d906a
Jason Rouse <jrrouse88@gmail.com>
2017-11-06 15:09:21 -0800

    links added to navbar

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 const NavBar = (props) => {
   return (
     <div className='navbar'>
-      This is the nav bar
+      <Link to="/">Home</Link>
+      <Link to="/podcasts">Shows</Link>
+      <Link to="/users">Hosts</Link>
     </div>
   )
 }
