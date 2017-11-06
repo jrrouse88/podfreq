a84e88699e4ff8bd22d858287b7cca8c93d051dc
Jason Rouse <jrrouse88@gmail.com>
2017-11-06 01:15:54 -0800

    episode model updated with comment embeded model and podcast reference

--- models/Episode.js
+++ models/Episode.js
@@ -1,10 +1,16 @@
 const
 mongoose = require('mongoose'),
+commentSchema = new mongoose.Schema({
+  title: String,
+  body: String
+})
 episodeSchema = new mongoose.Schema({
   title: {type: String, required: true},
   description: String,
   length: Number,
-  thumbnail: String
+  thumbnail: String,
+  podcast: {type: mongoose.Schema.Types.ObjectId, ref: 'Podcast'},
+  comments: [commentSchema]
 })
 ;
 
