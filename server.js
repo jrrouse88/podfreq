222325466d6eba7ffbf0d00864f8d9fca060871e
Jason Rouse <jrrouse88@gmail.com>
2017-11-06 00:42:33 -0800

    all server requires and connections

---
+++ server.js
@@ -0,0 +1,21 @@
+const
+  dotenv = require('dotenv'),
+  express = require('express'),
+  logger = require('morgan'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  app = express(),
+  MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/podfreq',
+  PORT = process.env.PORT || 3001
+;
+
+mongoose.connect(MONGODB_URI, (err) => {
+  console.log(err || `🖖🏾 Connected to MongoDB @ ${MONGODB_URI}`)
+})
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
+app.listen(PORT, (err) => {
+  console.log(err || `🤘🏾 Server running on ${PORT}`)
+})
