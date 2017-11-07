b1395006137f1e9626a6707651b2ad0067ddb0c1
Jason Rouse <jrrouse88@gmail.com>
2017-11-07 12:14:50 -0800

    added input change and submit methods

--- client/src/views/SignUp.jsx
+++ client/src/views/SignUp.jsx
@@ -15,19 +15,32 @@ class SignUp extends Component {
 
   onFormSubmit(evt) {
     evt.preventDefault()
-    console.log(this.state.fields)
+    axios({method: 'post', url:'/users', data: this.state.fields})
+      .then(res => {
+        console.log(res.data)
+      })
+  }
+
+  onInputChange(evt) {
+    console.log(evt.target)
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        [evt.target.name]: evt.target.value
+      }
+    })
   }
 
   render() {
     return (
       <div className="signUpContainer">
         <h1>Sign Up</h1>
-        <form>
+        <form onChange={this.onInputChange.bind(this)} onSubmit={this.onFormSubmit.bind(this)}>
           <input type="text" placeholder="First Name" name="firstName"/>
           <input type="text" placeholder="Last Name" name="lastName"/>
           <input type="text" placeholder="Username" name="userName"/>
           <input type="text" placeholder="Email" name="email"/>
-          <input type="text" placeholder="Password" name="password"/>
+          <input type="password" placeholder="Password" name="password"/>
           <input type="text" placeholder="Profile Picture" name="avatar"/>
           <button>Create Account</button>
         </form>
