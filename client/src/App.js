33708906b49fbba568d24ca9ad792cd72f280cdc
Jason Rouse <jrrouse88@gmail.com>
2017-11-07 15:17:45 -0800

    added mount for setting currentUser

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom'
+import clientAuth from './clientAuth'
 
 import NavBar from './NavBar.jsx'
 import Shows from './views/Shows'
@@ -7,7 +8,27 @@ import Hosts from './views/Hosts'
 import SignUp from './views/SignUp'
 
 class App extends Component {
+  state = { currentUser: null }
+
+  componentDidMount() {
+    this.setState({
+      currentUser: clientAuth.getCurrentUser()
+    })
+  }
+
+  onLoginSuccess(user) {
+    this.setState({
+      currentUser: clientAuth.getCurrentUser()
+    })
+  }
+
+  logOut() {
+    clientAuth.logOut()
+		this.setState({ currentUser: null })
+  }
+
   render() {
+    const { currentUser } = this.state
     return (
       <div className="App">
         <NavBar />
