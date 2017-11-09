694a13766fb26b1b5ad3e16586ce1cbae9b8ca2f
Jason Rouse <jrrouse88@gmail.com>
2017-11-08 16:42:29 -0800

    added bulma

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
+import 'bulma/css/bulma.css'
 import registerServiceWorker from './registerServiceWorker'
 
 import App from './App'
