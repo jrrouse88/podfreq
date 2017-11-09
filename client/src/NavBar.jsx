d956e1854064c4e33691e2de1cef034f37204c14
Jason Rouse <jrrouse88@gmail.com>
2017-11-08 17:00:32 -0800

    quick navbar styling

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -3,14 +3,18 @@ import { Link } from 'react-router-dom'
 
 const NavBar = (props) => {
   return (
-    <div className='navbar'>
-      <Link to="/">Home</Link>
-      <Link to="/podcasts">Shows</Link>
-      <Link to="/hosts">Hosts</Link>
-      <Link to="/login">Log In</Link>
-      <Link to="/signup">Sign Up</Link>
-      <Link to="/profile">Profile</Link>
-      <Link to="/logout">Log Out</Link>
+    <div className='navbar is-fixed-top is-black'>
+      <Link to="/" className="navbar-brand">PodFreq</Link>
+      <div className="navbar-menu">
+        <div className="navbar-end">
+          <Link to="/podcasts">Shows</Link>
+          <Link to="/hosts">Hosts</Link>
+          <Link to="/login">Log In</Link>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/profile">Profile</Link>
+          <Link to="/logout">Log Out</Link>
+        </div>
+      </div>
     </div>
   )
 }
