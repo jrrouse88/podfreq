0e7207772adca0c735fbcd78c4392f87cdeecc02
Jason Rouse <jrrouse88@gmail.com>
2017-11-13 14:57:32 -0800

    commenting code

--- server.js
+++ server.js
@@ -5,33 +5,42 @@ const
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
   app = express(),
+  // set a variable to connect to mongoDB for deployment or development
   MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/podfreq',
+  // set a variable to connect to express app for deployment or development
   PORT = process.env.PORT || 3001,
+  // import routes
   usersRoutes = require('./routes/users.js'),
   podcastRoutes = require('./routes/podcasts.js'),
   episodeRoutes = require('./routes/episodes.js')
 ;
 
+// connect to mongoDB
 mongoose.connect(MONGODB_URI, (err) => {
   console.log(err || `🖖🏾 Connected to MongoDB @ ${MONGODB_URI}`)
 })
 
+//set up middle ware for deployment, morgan, and parser
 app.use(express.static(`${__dirname}/client/build`))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 
+// set server root URL
 app.get('/', (req, res) => {
   res.json({message: "API root."})
 })
 
+// use routes for various models
 app.use('/users', usersRoutes)
 app.use('/podcasts', podcastRoutes)
 app.use('/episodes', episodeRoutes)
 
+// middleware to help with deployment
 app.use('*', (req, res) => {
 	res.sendFile(`${__dirname}/client/build/index.html`)
 })
 
+// connect to express server
 app.listen(PORT, (err) => {
   console.log(err || `🤘🏾 Server running on ${PORT}`)
 })
