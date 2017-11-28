2c01c64c702e6209566f0e164115e38f8ea803dc
Jason Rouse <jrrouse88@gmail.com>
2017-11-28 09:56:03 -0800

    added bg image and sizing to component

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Home = (props) => {
 	return (
-		<div className='container'>
-			<div className='jumbotron text-center'>
-				<h1 className='display-3'>PodFreq</h1>
-				<p className='lead'>A site for podcasters to share their passion.</p>
-				<button className='btn btn-primary btn-lg'>Sign Up</button>
-				<button className='btn btn-secondary btn-lg'>Log In</button>
+		<div className='home-body'>
+			<div className='jumbo-contain'>
+				<div className='jumbotron text-center'>
+					<h1 className='display-2'>PodFreq</h1>
+					<p className='lead'>A site for podcasters to share their passion.</p>
+					<button className='btn btn-primary btn-lg'>Sign Up</button>
+					<button className='btn btn-secondary btn-lg'>Log In</button>
+				</div>
 			</div>
 		</div>
 	)
