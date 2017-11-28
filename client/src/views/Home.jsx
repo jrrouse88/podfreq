92b396808fb7dfd42b4fab3fde8a35fcc2064af1
Jason Rouse <jrrouse88@gmail.com>
2017-11-28 10:06:48 -0800

    added button links

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = (props) => {
 	return (
@@ -7,8 +8,8 @@ const Home = (props) => {
 				<div className='jumbotron text-center'>
 					<h1 className='display-2'>PodFreq</h1>
 					<p className='lead'>A site for podcasters to share their passion.</p>
-					<button className='btn btn-primary btn-lg'>Sign Up</button>
-					<button className='btn btn-secondary btn-lg'>Log In</button>
+					<Link to="/signup"><button className='btn btn-primary btn-lg'>Sign Up</button></Link>
+					<Link to="/login"><button className='btn btn-secondary btn-lg'>Log In</button></Link>
 				</div>
 			</div>
 		</div>
