c1b628139ba7bfc060ce74db3c51c6dc3c870d42
Jason Rouse <jrrouse88@gmail.com>
2017-11-28 09:16:59 -0800

    added bootstrap link and jumbotron

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Home = (props) => {
 	return (
-		<div className='Home'>
-			<h1>Home Page</h1>
+		<div className='container'>
+			<div className='jumbotron text-center'>
+				<h1 className='display-3'>PodFreq</h1>
+				<p className='lead'>A site for podcasters to share their passion.</p>
+				<button className='btn btn-primary btn-lg'>Sign Up</button>
+				<button className='btn btn-secondary btn-lg'>Log In</button>
+			</div>
 		</div>
 	)
 }
